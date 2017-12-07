51ea26c88a86149ceb6ec5f673521dc5f2d403c5
Taimoor Naveed <t.naveed@isofleet.com>
2017-12-07 15:22:42 +0100

    pwm 30% on PD1

--- blink.c
+++ blink.c
@@ -21,31 +21,39 @@
 #include "em_emu.h"
 #include "bsp.h"
 #include "bsp_trace.h"
+#include "em_gpio.h"
+#include "em_timer.h"
+#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_timer.c"
 
-volatile uint32_t msTicks; /* counts 1ms timeTicks */
+#define DUTY_CYCLE                  6		// for 30%
+#define TIMER_TOP                   20
+#define TIMER_CHANNEL				0
+#define TIMER_NUMBER				TIMER0
 
-void Delay(uint32_t dlyTicks);
-
-/**************************************************************************//**
- * @brief SysTick_Handler
- * Interrupt Service Routine for system tick counter
- *****************************************************************************/
-void SysTick_Handler(void)
-{
-  msTicks++;       /* increment counter necessary in Delay()*/
-}
-
-/**************************************************************************//**
- * @brief Delays number of msTick Systicks (typically 1 ms)
- * @param dlyTicks Number of ticks to delay
- *****************************************************************************/
-void Delay(uint32_t dlyTicks)
-{
-  uint32_t curTicks;
-
-  curTicks = msTicks;
-  while ((msTicks - curTicks) < dlyTicks) ;
-}
+//volatile uint32_t msTicks; /* counts 1ms timeTicks */
+//
+//void Delay(uint32_t dlyTicks);
+//
+///**************************************************************************//**
+// * @brief SysTick_Handler
+// * Interrupt Service Routine for system tick counter
+// *****************************************************************************/
+//void SysTick_Handler(void)
+//{
+//  msTicks++;       /* increment counter necessary in Delay()*/
+//}
+//
+///**************************************************************************//**
+// * @brief Delays number of msTick Systicks (typically 1 ms)
+// * @param dlyTicks Number of ticks to delay
+// *****************************************************************************/
+//void Delay(uint32_t dlyTicks)
+//{
+//  uint32_t curTicks;
+//
+//  curTicks = msTicks;
+//  while ((msTicks - curTicks) < dlyTicks) ;
+//}
 
 /**************************************************************************//**
  * @brief  Main function
@@ -55,21 +63,40 @@ int main(void)
   /* Chip errata */
   CHIP_Init();
 
-  /* If first word of user data page is non-zero, enable eA Profiler trace */
-  BSP_TraceProfilerSetup();
+  /* Initialize gpio pin */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+  GPIO_PinModeSet(gpioPortD, 1, gpioModePushPull, 0);
 
-  /* Setup SysTick Timer for 1 msec interrupts  */
-  if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
 
-  /* Initialize LED driver */
-  BSP_LedsInit();
-  BSP_LedSet(0);
 
-  /* Infinite blink loop */
+  CMU_ClockEnable(cmuClock_TIMER0, true);
+
+  // Create the timer count control object initializer
+  TIMER_InitCC_TypeDef timerCCInit = TIMER_INITCC_DEFAULT;
+  timerCCInit.mode = timerCCModePWM;
+  timerCCInit.cmoa = timerOutputActionToggle;
+
+  // Configure CC channel 0
+  TIMER_InitCC(TIMER_NUMBER, TIMER_CHANNEL, &timerCCInit);
+
+  // Route CC0 to location 3 (PD1) and enable pin for cc0
+  TIMER3->ROUTE |= (TIMER_ROUTE_CC0PEN | TIMER_ROUTE_LOCATION_LOC3);
+
+  // Set Top Value
+  TIMER_TopSet(TIMER_NUMBER, TIMER_TOP);
+
+  // Set the PWM duty cycle here!
+  TIMER_CompareBufSet(TIMER_NUMBER, TIMER_CHANNEL, DUTY_CYCLE);
+
+  // Create a timerInit object, based on the API default
+  TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
+  timerInit.prescale = timerPrescale4;
+
+  TIMER_Init(TIMER_NUMBER, &timerInit);
+
   while (1)
   {
-    BSP_LedToggle(0);
-    BSP_LedToggle(1);
-    Delay(2000);
+
   }
 }
+
