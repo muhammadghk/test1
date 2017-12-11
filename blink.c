ead8c603b624d535094ac005655b93bb3bc2b923
Taimoor Naveed <t.naveed@isofleet.com>
2017-12-11 11:30:36 +0100

    voltage level indicator by leds

--- blink.c
+++ blink.c
@@ -17,11 +17,22 @@
 #include <stdbool.h>
 #include "em_device.h"
 #include "em_chip.h"
+#include "em_system.h"
 #include "em_cmu.h"
 #include "em_emu.h"
 #include "bsp.h"
 #include "bsp_trace.h"
+#include "em_gpio.h"
+//#include "em_adc.h"
+//#include "em_timer.h"
+#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_timer.c"
+#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_adc.c"
 
+
+uint8_t 	counter		= 0;
+uint32_t 	DUTY_CYCLE	= 0;
+uint32_t 	TIMBUF;
+float 		voltage=0;
 volatile uint32_t msTicks; /* counts 1ms timeTicks */
 
 void Delay(uint32_t dlyTicks);
@@ -46,30 +57,81 @@ void Delay(uint32_t dlyTicks)
   curTicks = msTicks;
   while ((msTicks - curTicks) < dlyTicks) ;
 }
+ static void ADCConfig(void)
+ {
+   ADC_Init_TypeDef       init       = ADC_INIT_DEFAULT;
+   ADC_InitSingle_TypeDef singleInit = ADC_INITSINGLE_DEFAULT;
+
+   init.timebase = ADC_TimebaseCalc(0);
+   init.prescale = ADC_PrescaleCalc(7000000, 0);
+   ADC_Init(ADC0, &init);
+
+   /* Init for single conversion use, measure VDD/3 with 1.25 reference. */
+   singleInit.reference  = adcRef5VDIFF;
+   singleInit.input      = adcSingleInpCh0;
+   singleInit.resolution = adcRes12Bit;
+   //singleInit.rep		= true;
+   singleInit.acqTime = adcAcqTime64;
+
+   ADC_InitSingle(ADC0, &singleInit);
+ }
 
 /**************************************************************************//**
  * @brief  Main function
  *****************************************************************************/
 int main(void)
 {
+
+
+  uint32_t sample;
   /* Chip errata */
   CHIP_Init();
 
-  /* If first word of user data page is non-zero, enable eA Profiler trace */
-  BSP_TraceProfilerSetup();
+  if (SysTick_Config(CMU_ClockFreqGet(cmuClock_HFPER) / 1000)) while (1) ;
 
-  /* Setup SysTick Timer for 1 msec interrupts  */
-  if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
+  /* Initialize peripherals clock */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  CMU_ClockEnable(cmuClock_ADC0, true);
+
+
+//  GPIO_PinModeSet(gpioPortD, 0 , gpioModeInput, 0);
+
 
   /* Initialize LED driver */
   BSP_LedsInit();
   BSP_LedSet(0);
 
-  /* Infinite blink loop */
+  ADCConfig();
+
+
   while (1)
   {
-    BSP_LedToggle(0);
-    BSP_LedToggle(1);
-    Delay(2000);
-  }
+	    ADC_Start(ADC0, adcStartSingle);
+
+	    while (ADC0->STATUS & ADC_STATUS_SINGLEACT) ;
+
+		sample = ADC_DataSingleGet(ADC0);
+		voltage = sample * 5;
+		voltage /= 4096;
+		if(voltage <  2.6 && voltage > 0)
+		{
+			BSP_LedClear(1);
+			BSP_LedSet(0);
+
+		}
+		if(voltage >= 2.6 && voltage < 3.4)
+		{
+			BSP_LedSet(1);
+			BSP_LedClear(0);
+
+		}
+		if(voltage >= 3.4)
+		{
+			BSP_LedSet(0);
+			BSP_LedSet(1);
+		}
+	}
+
 }
+
