0c1769bfe80764cd72ef45505345b65b1fd96dba
Taimoor Naveed <t.naveed@isofleet.com>
2017-12-12 13:33:45 +0100

    ACMP and polling mode

--- blink.c
+++ blink.c
@@ -23,16 +23,16 @@
 #include "bsp.h"
 #include "bsp_trace.h"
 #include "em_gpio.h"
-//#include "em_adc.h"
-//#include "em_timer.h"
-#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_timer.c"
-#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_adc.c"
+//#include "em_dac.h"
+//#include "em_acmp.h"
+#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_dac.c"
+#include "/home/taimoor/SimplicityStudio_v4/developer/sdks/exx32/v4.4.1/emlib/src/em_acmp.c"
 
 
 uint8_t 	counter		= 0;
 uint32_t 	DUTY_CYCLE	= 0;
 uint32_t 	TIMBUF;
-float 		voltage=0;
+float 		voltage1=0, voltage0=0;
 volatile uint32_t msTicks; /* counts 1ms timeTicks */
 
 void Delay(uint32_t dlyTicks);
@@ -57,24 +57,35 @@ void Delay(uint32_t dlyTicks)
   curTicks = msTicks;
   while ((msTicks - curTicks) < dlyTicks) ;
 }
- static void ADCConfig(void)
+ static void ACMPConfig(void)
  {
-   ADC_Init_TypeDef       init       = ADC_INIT_DEFAULT;
-   ADC_InitSingle_TypeDef singleInit = ADC_INITSINGLE_DEFAULT;
+   ACMP_Init_TypeDef acmp_init    = ACMP_INIT_DEFAULT;
 
-   init.timebase = ADC_TimebaseCalc(0);
-   init.prescale = ADC_PrescaleCalc(7000000, 0);
-   ADC_Init(ADC0, &init);
+   CMU_ClockEnable(cmuClock_ACMP0, true);
 
-   /* Init for single conversion use, measure VDD/3 with 1.25 reference. */
-   singleInit.reference  = adcRef5VDIFF;
-   singleInit.input      = adcSingleInpCh0;
-   singleInit.resolution = adcRes12Bit;
-   //singleInit.rep		= true;
-   singleInit.acqTime = adcAcqTime64;
-
-   ADC_InitSingle(ADC0, &singleInit);
+   ACMP_Init(ACMP0, &acmp_init);
+   ACMP_ChannelSet(ACMP0, acmpChannelDAC0Ch1, acmpChannel6);
  }
+
+ static void DACConfig(void)
+ {
+	  /* Use default settings */
+	  DAC_Init_TypeDef        init        = DAC_INIT_DEFAULT;
+	  DAC_InitChannel_TypeDef initChannel = DAC_INITCHANNEL_DEFAULT;
+
+	  CMU_ClockEnable(cmuClock_DAC0, true);
+
+
+	  init.prescale = DAC_PrescaleCalc(500000, 0);
+	  init.reference = dacRef1V25;
+	  init.outMode = dacOutputPinADC;
+	  /* Initialize the DAC and DAC channel. */
+	  DAC_Init(DAC0, &init);
+
+	  DAC_InitChannel(DAC0, &initChannel, 0);
+	  DAC_InitChannel(DAC0, &initChannel, 1);
+ }
+
 
 /**************************************************************************//**
  * @brief  Main function
@@ -83,53 +94,65 @@ int main(void)
 {
 
 
-  uint32_t sample;
+  uint32_t DAC_Value;
   /* Chip errata */
   CHIP_Init();
 
   if (SysTick_Config(CMU_ClockFreqGet(cmuClock_HFPER) / 1000)) while (1) ;
 
-  /* Initialize peripherals clock */
+  CMU_ClockEnable(cmuClock_HFPER, true);
   CMU_ClockEnable(cmuClock_GPIO, true);
-
-  CMU_ClockEnable(cmuClock_ADC0, true);
-
-
-//  GPIO_PinModeSet(gpioPortD, 0 , gpioModeInput, 0);
-
+  GPIO_PinModeSet(gpioPortB,9,gpioModeInputPull,1);
+  GPIO_PinModeSet(gpioPortB,10,gpioModeInputPull,1);
 
   /* Initialize LED driver */
   BSP_LedsInit();
-  BSP_LedSet(0);
 
-  ADCConfig();
-
+  DACConfig();
+  ACMPConfig();
+  ACMP_Enable(ACMP0);
+  DAC_Enable(DAC0,0,1);
+  DAC_Enable(DAC0,1,1);
 
   while (1)
   {
-	    ADC_Start(ADC0, adcStartSingle);
 
-	    while (ADC0->STATUS & ADC_STATUS_SINGLEACT) ;
-
-		sample = ADC_DataSingleGet(ADC0);
-		voltage = sample * 5;
-		voltage /= 4096;
-		if(voltage <  2.6 && voltage > 0)
+	    if( !(GPIO_PinInGet(gpioPortB, 9)) )
 		{
-			BSP_LedClear(1);
-			BSP_LedSet(0);
-
+	    	Delay(100);
+		    if( !(GPIO_PinInGet(gpioPortB, 9)) )
+		    {
+				voltage0+=0.1;
+				if(voltage0>1.25) voltage0=0;
+				DAC_Value = (uint32_t)((voltage0 * 4096) / 1.25);
+				DAC_ChannelOutputSet(DAC0,0,DAC_Value);
+	//			BSP_LedSet(1);
+	//			BSP_LedClear(0);
+		    }
+	    }
+	    if( !(GPIO_PinInGet(gpioPortB, 10)) )
+		{
+	    	Delay(100);
+		    if( !(GPIO_PinInGet(gpioPortB, 10)) )
+		    {
+				voltage1+=0.1;
+				if(voltage1>1.25) voltage1=0;
+				DAC_Value = (uint32_t)((voltage1 * 4096) / 1.25);
+				DAC_ChannelOutputSet(DAC0,1,DAC_Value);
+	//			BSP_LedClear(1);
+	//			BSP_LedSet(0);
+		    }
 		}
-		if(voltage >= 2.6 && voltage < 3.4)
+
+		if(ACMP0->STATUS & ACMP_STATUS_ACMPOUT)
 		{
 			BSP_LedSet(1);
 			BSP_LedClear(0);
-
 		}
-		if(voltage >= 3.4)
+		else
 		{
+			BSP_LedClear(1);
 			BSP_LedSet(0);
-			BSP_LedSet(1);
 		}
 	}
 
